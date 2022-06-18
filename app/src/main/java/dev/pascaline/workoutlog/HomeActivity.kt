dcd36135b933b6d2c2c80428e6d5ddbb5cba4153
Pascaline-Umulisa <kayiteteumulisa@gmail.com>
2022-06-18 09:01:35 +0300

    cast views for fragments

--- app/src/main/java/dev/pascaline/workoutlog/HomeActivity.kt
+++ app/src/main/java/dev/pascaline/workoutlog/HomeActivity.kt
@@ -2,10 +2,58 @@ package dev.pascaline.workoutlog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
+
+    lateinit var bnvHome: BottomNavigationView
+    lateinit var fcvHome: FragmentContainerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        castViews()
+        setupBottomNav()
+    }
+
+    fun castViews() {
+        fcvHome = findViewById(R.id.fcvHome)
+        bnvHome = findViewById(R.id.bnvHome)
+    }
+
+    fun setupBottomNav() {
+        bnvHome.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.plan -> {
+                    val transaction = supportFragmentManager.beginTransaction().replace(R.id.fcvHome, PlanFragment()).commit()
+                    true
+                }
+                R.id.track -> {
+                    val transaction = supportFragmentManager.beginTransaction().replace(R.id.fcvHome, TrackFragment()).commit()
+                    true
+                }
+                R.id.profile -> {
+
+                    supportFragmentManager.beginTransaction().replace(R.id.fcvHome, ProfileFragment()).commit()
+                    true
+
+                }
+                else -> false
+
+
+            }
+        }
     }
 }
+
+
+
+//when (item.itemId) {
+//    R.id.plan -> {
+//        val transaction = supportFragmentManager.beginTransaction()
+//        transaction.replace(R.id.fcvHome, PlanFragment())
+//        transaction.commit()
+//        true
+//    }
+//}
