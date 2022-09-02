e6bc1cfa15282f9b0464c988c63d952140dd400e
Pascaline-Umulisa <kayiteteumulisa@gmail.com>
2022-09-02 19:22:57 +0300

    registering an account

--- app/src/main/java/dev/pascaline/workoutlog/retrofit/ApiInterface.kt
+++ app/src/main/java/dev/pascaline/workoutlog/retrofit/ApiInterface.kt
@@ -7,6 +7,6 @@ import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface ApiInterface {
-    @POST("/regiter")
+    @POST("/register")
     fun registerUser(@Body registerRequest: RegisterRequest):Call<RegisterResponse>
 }
